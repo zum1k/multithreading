63100909fce33e3cc9631378f813122379c1cf08
Your Name <you@example.com>
2020-05-07 09:56:06 +0300

    blockdques - dqueue

--- src/main/java/com/epam/multithreading/seaport/SeaPort.java
+++ src/main/java/com/epam/multithreading/seaport/SeaPort.java
@@ -3,9 +3,7 @@ package com.epam.multithreading.seaport;
 import com.epam.multithreading.entity.Container;
 import com.epam.multithreading.entity.Ship;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Semaphore;
@@ -15,8 +13,8 @@ public class SeaPort {
     private static final int PORT_CONTAINER_CAPACITY = 20000;
     private static final int DOCKS_NUMBER = 6;
     private static final int WATER_AREA_CAPACITY = 50;
-    private final BlockingQueue<Ship> waterAreaQueue = new ArrayBlockingQueue<Ship>(WATER_AREA_CAPACITY);
-    private final BlockingQueue<Dock> dockQueue = new ArrayBlockingQueue<>(DOCKS_NUMBER);
+    private final Deque<Ship> waterAreaQueue = new ArrayDeque<>();
+    private final Deque<Dock> dockQueue = new ArrayDeque<>();
     private Map<Integer, Dock> docks;
     private List<Container> containers = new ArrayList<>();
     Semaphore dockCapacity = new Semaphore(DOCKS_NUMBER);
